50be52dd63e8aee33e26edad01cb0ba64c662047
saikrishna110 <saikrishnakadali552@gmail.com>
2024-06-10 10:41:35 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,26 @@
+"""
+Write a Program to retrieve character 'X' using positive indexing
+from "Semantic segmentation of Lung Cancer from Chest X-Ray" string
+"""
+
+str1="Semantic segmentation of Lung Cancer from Chest X-Ray"
+print(str1[48])
+
+'''
+Write a Program to retrieve the 'L' character from "I have to Love Python Programming" string
+using negative/reverse indexing
+'''
+str2="I have to Love Python Programming"
+print(str2[-23])
+
+'''
+Can prove that python string is immutable with the program
+'''
+
+str3="i love python"
+str3[0]='s'
+print(str3)
+
+
+
+
